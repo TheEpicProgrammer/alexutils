a1a115a95c7b6556a57933a947aedcaea4bf66da
Alexander Park <parkalex380@gmail.com>
2021-06-15 08:33:24 -0500

    change most example stuff

---
+++ src/main/java/me/parkalex/alexutils/AlexUtils.java
@@ -0,0 +1,11 @@
+package me.parkalex.alexutils;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.client
+
+public class AlexUtils implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("alexutils has been initialized!");
+	}
+}
